ac1389d3128386a3cf22b46d606341cfd78b1cfc
Dave Pernitz <pernitz@uwalumni.com>
2020-01-31 17:34:20 -0700

    Add function to nav buttons

--- main.js
+++ main.js
@@ -5,5 +5,29 @@ var closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', disappear);
 
  function disappear() {
-   closeWindow.classList.add('closed');
+   closeWindow.classList.add('hide');
  };
+
+
+var clipboard = document.querySelector('.clipboard');
+var wallet = document.querySelector('.wallet');
+var main = document.querySelector('.main');
+
+wallet.addEventListener('click', borderToWallet)
+// wallet.addEventListener('click', addNewView)
+
+function borderToWallet() {
+  clipboard.classList.remove('nav-image-border')
+  wallet.classList.add('nav-image-border')
+  main.classList.add('hide')
+}
+
+clipboard.addEventListener('click', borderToClipboard)
+
+function borderToClipboard() {
+  wallet.classList.remove('nav-image-border')
+  clipboard.classList.add('nav-image-border')
+  main.classList.remove('hide')
+}
+
+  // main.insertAdjacentHTML('beforeend', '<h2 clas="new-heading">HELLO</h2>')
