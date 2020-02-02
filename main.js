c36678ee67419a478c47b4305f29095ae0e94367
Dave Pernitz <pernitz@uwalumni.com>
2020-02-02 13:19:31 -0700

    Modify css to match comp (code needs clean)

--- main.js
+++ main.js
@@ -13,23 +13,21 @@ var clipboard = document.querySelector('.clipboard');
 var wallet = document.querySelector('.wallet');
 var main = document.querySelector('.main');
 var main2 = document.querySelector('.main2');
-wallet.addEventListener('click', borderToWallet)
-// wallet.addEventListener('click', addNewView)
+
+wallet.addEventListener('click', borderToWallet);
 
 function borderToWallet() {
-  clipboard.classList.remove('nav-image-border')
-  wallet.classList.add('nav-image-border')
-  main.classList.add('hide')
-  main2.classList.remove('hide')
+  clipboard.classList.remove('nav-image-border');
+  wallet.classList.add('nav-image-border');
+  main.classList.add('hide');
+  main2.classList.remove('hide');
 }
 
 clipboard.addEventListener('click', borderToClipboard)
 
 function borderToClipboard() {
-  wallet.classList.remove('nav-image-border')
-  clipboard.classList.add('nav-image-border')
-  main.classList.remove('hide')
-  main2.classList.add('hide')
+  wallet.classList.remove('nav-image-border');
+  clipboard.classList.add('nav-image-border');
+  main.classList.remove('hide');
+  main2.classList.add('hide');
 }
-
-  // main.insertAdjacentHTML('beforeend', '<h2 clas="new-heading">HELLO</h2>')
